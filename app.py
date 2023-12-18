99609943a6a1976437b5bfc3dd73aa57ad690292
Kylie Wise <54920689+kylietwise@users.noreply.github.com>
2023-12-18 12:00:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -18,10 +20,13 @@ def choose_credit_card(cards, purchase_amount, category):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    selected_card = None
+    category = None
+
     if request.method == 'POST':
         purchase_amount = float(request.form['purchase_amount'])
         category = request.form['category']
-        
+
         # Example credit cards and their rewards rates for different categories (as percentages)
         credit_cards = {
         "Bilt": {"dining": 5,"travel": 2,"lyft": 3, "rent": 1, "shopping": 1},
@@ -33,9 +38,11 @@ def index():
         # Choose the best credit card for the given category
         selected_card = choose_credit_card(credit_cards, purchase_amount, category)
 
-        return render_template('index.html', selected_card=selected_card, category=category)
+        # Generate static HTML content
+        with open("docs/index.html", "w") as f:
+            f.write(f"<p>Use {selected_card} for maximum rewards in the {category} category.</p>" if selected_card else f"<p>No cards available for the {category} category.</p>")
 
-    return render_template('index.html', selected_card=None, category=None)
+    return render_template('index.html', selected_card=selected_card, category=category)
 
 if __name__ == '__main__':
     app.run(debug=True)
